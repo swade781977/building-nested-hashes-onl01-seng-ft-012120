d3b6346516c9a50723232acab6722ecf4a67fe61
Chris Wade <swade781977@yahoo.com>
2020-01-27 16:22:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch{}
+      :matriarch{}
+      :hero{}
+      :hearo_friends[]
       }, 
     :capulet => {
       #your key/value pairs here!
